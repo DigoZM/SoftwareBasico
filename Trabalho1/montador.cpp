e3aa4d2ae00c311c22016f7b19fe90cc8759cf45
Rodrigo Zamagno <31426233+DigoZM@users.noreply.github.com>
2021-08-27 09:35:27 -0300

    Data section complete and text section without labels

--- Trabalho1/montador.cpp
+++ Trabalho1/montador.cpp
@@ -19,6 +19,8 @@ using namespace std;
 #define input 12
 #define output 13
 #define stop 14
+#define constData 15
+#define space 16 
 
 struct SymbolTableElements
 {
@@ -46,6 +48,13 @@ void print_vector(vector<int> v){
     cout << v[i] << ' ';
   }
   cout << endl;
+}
+
+void print_ST(vector<SymbolTableElements> v){
+  cout << "Symbol Table" << endl;
+  for(int i=0; i<v.size(); ++i){
+    cout << v[i].symbol << " " << v[i].value << " " << v[i].def << " " << v[i].list << endl;
+  }
 }
 
 // END OF DEBUG FUNCTIONS
@@ -106,6 +115,12 @@ int token_table(string token){
   {
     return stop;
   }
+  if (token.compare("const") == 0){
+    return constData;
+  }
+  if (token.compare("space") == 0){
+    return space;
+  }
   //opcode incorreto
   return -1;
 }
@@ -116,31 +131,53 @@ void one_pass_algorithm(char* fileName){
   ifstream MyReadFile(fileName);
   int lineCount = 0;
   int posCount = 0;
-  string token;
+  string token; 
   vector<int> code;
-  int opcode;
+  int opcode, symbol, aux;
   vector<SymbolTableElements> symbolTable;
+  bool opcodeFlag = false;
+  bool dataSection = false;
+  bool textSection = false;
+  bool constFlag = false;
+  bool newSymbol = false;
+  string symbolFound = "";
 
 
   while (getline (MyReadFile, myText)) { // Read line by line till it reaches end of file
     std::transform(myText.begin(), myText.end(), myText.begin(), ::tolower);
 
     lineCount++;
-
+    cout << myText << endl;
     if(myText.compare("section text") == 0){
-      cout << "entrei na text section" << '\n';
+      cout << "entrei na text section" << '\n' << '\n';
+      print_vector(code);
+      print_ST(symbolTable);
+      textSection = true;
+      dataSection = false;
       getline (MyReadFile, myText);
       std::transform(myText.begin(), myText.end(), myText.begin(), ::tolower);
       lineCount++;
+    }
+    if(myText.compare("section data") == 0){
+      cout << "entrei na data section" << '\n' << '\n';
+      print_vector(code);
+      print_ST(symbolTable);
+      cout  << endl;
+      textSection = false;
+      dataSection = true;
+      getline (MyReadFile, myText);
+      std::transform(myText.begin(), myText.end(), myText.begin(), ::tolower);
+      lineCount++;
+    }
 
-      while (myText.compare("section data") != 0 && MyReadFile.eof() != true)
-      {
-
+    if(textSection){
+        myText = myText + "\n";
         token = "";
         for (auto x: myText){
 
-          if(x == ' '){
+          if(x == ' ' && opcodeFlag == false){
             //token
+            
             opcode = token_table(token);
             switch (opcode){
               case add:
@@ -185,48 +222,148 @@ void one_pass_algorithm(char* fileName){
               case stop:
                 code.push_back(stop);
                 break;
-              case -1:
+              default:
                 cout << "erro no opcode na linha" << lineCount << endl;
                 cout << token << endl;
-              default:
+                posCount--;
                 break;
             }
+            cout << "achei opcode: " << token << endl;
             token = "";
             posCount++;
+            opcodeFlag = true;
           }
-          if(x == ':'){
+          else if(x == ':'){
             //rotulo
+            cout << "achei rotulo: " << token << endl;
             auto search = find_if(symbolTable.begin(), symbolTable.end(), 
                        [&token] (SymbolTableElements const& d) { 
                           return d.symbol == token; 
                        });
             if (search != symbolTable.end()){
-              cout << "erro rotulo ja definido" << endl;
+              cout << "erro rotulo ja definido na linha: " << lineCount << endl;
             } else {
               SymbolTableElements element = {token, posCount, true, -1};
               symbolTable.push_back(element);
             }
-            posCount++;
+            //posCount++;
             cout << token << endl;
             token = "";
           }
+          else if((x == ' ' || x == '\n' || x == ',') && opcodeFlag == true){
+            cout << "achei simbolo: " << token << endl;
+            //symbol
+            //search for symbol in ST
+            auto search = find_if(symbolTable.begin(), symbolTable.end(), 
+                       [&token] (SymbolTableElements const& d) { 
+                          return d.symbol == token; 
+                       });
+            if (search != symbolTable.end()){
+              cout << "simbolo ja definido" << endl;
+              if(search->def){
+                cout << "tem posição" << endl;
+                code.push_back(search->value);
+                posCount++;
+              } else {
+                cout << "nao tem posição" << endl;
+                int prevPos = search->list;
+                code.push_back(prevPos);
+                search->list = posCount;
+                posCount++;
+              }
+            } else {
+              cout << "simbolo ainda nao definido" << endl;
+              SymbolTableElements element = {token, -1, false, posCount};
+              symbolTable.push_back(element);
+              code.push_back(-1);
+              posCount++;
+            }
+            token = "";
+            opcodeFlag = false;
+          }
           else {
             token = token + x;
-            cout << '\"' <<  token << '\"'<< endl;
+            //cout << '\"' <<  token << '\"'<< endl;
           }
+          //cout << "x atual \"" << x << "\""<< endl;
         }
         
-        //cout << lineCount << myText << '\n';
-        getline (MyReadFile, myText);
-        std::transform(myText.begin(), myText.end(), myText.begin(), ::tolower);
-        lineCount++;
+        //cout << lineCount << myText << '\n';      
+    }
+    
+    if(dataSection){
+      myText = myText + "\n";
+      token = "";
+      for (auto x: myText){
+        if(x == ':'){
+          //simbolo
+          cout << "DATA achei simbolo: " << token << endl;
+          auto search = find_if(symbolTable.begin(), symbolTable.end(), 
+                       [&token] (SymbolTableElements const& d) { 
+                          return d.symbol == token; 
+                       });
+          if (search != symbolTable.end()){
+            cout << "simbolo ja definido" << endl;
+            // FAZER ESSA PARTE DE COLOCAR NA TABELA O VALOR E IR PREENCHENDO OS VALORES
+            newSymbol = false;
+            symbolFound = search->symbol;
+            search->value = posCount;
+            search->def = true;
 
+          } else {
+            cout << "simbolo nao definido" << endl;
+            SymbolTableElements element = {token, posCount, true, -1};
+            symbolTable.push_back(element);
+            newSymbol = true;
+          }
+          token = "";
+        } else if(x == '\n' && constFlag){
+          //verificar se token é int
+          cout << "Valor do const: " << token << endl;
+          code.push_back(stoi(token));
+          posCount++;
+          constFlag = false;
+          token = "";
+        } else if((x == ' '|| x == '\n') && token != ":" && token != " " ){
+          cout << "vendo tipo: \"" << token << "\"" << endl;
+          symbol = token_table(token);
+          switch (symbol){
+            case constData:
+              constFlag = true;
+              break;
+            case space:
+              code.push_back(00);
+              posCount++;
+              break;
+          }
+          token = "";
+        } if(x == ' '){
+          token = "";
+        }
+         else {
+          token = token + x;
+        }
       }
-      
+      if(!newSymbol){
+        //print_ST(symbolTable);
+        auto search = find_if(symbolTable.begin(), symbolTable.end(), 
+                       [&symbolFound] (SymbolTableElements const& d) { 
+                          return d.symbol == symbolFound; 
+                       });
+        symbol = search->list;
+        //cout << "vou colocar o " << symbol << " no " << posCount << endl;
+        while (symbol != -1){
+          aux = symbol;
+          symbol = code[aux];
+          code[aux] = search->value;
+        }
+        newSymbol = false;
+      }
     }
 
   }
   print_vector(code);
+  print_ST(symbolTable);
   MyReadFile.close();
 }
 
